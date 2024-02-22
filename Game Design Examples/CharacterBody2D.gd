e20b09221de0bda2117abd62fd917592ff642580
SAW3production <105048323+SAW3production@users.noreply.github.com>
2024-02-22 14:49:35 +0000

    scene,player sprites, movement up and down

--- Game Design Examples/CharacterBody2D.gd
+++ Game Design Examples/CharacterBody2D.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const ROTATION = 100.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -14,15 +14,18 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	var rotation = Input.get_axis("ui_left","ui_right")
+	
+	if(rotation):
+		transform = transform.rotated_local(rotation)
+	
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("ui_up", "ui_down")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.y = direction * position.normalized().y * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.y = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
